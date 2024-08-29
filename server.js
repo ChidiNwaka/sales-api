de8dccbf1d7936f146bfcf1d3a2b86253cd234e4
Chidi Nwaka <chidi.nwaka@gmail.com>
2024-08-28 23:10:40 -0700

    added a json file to temporaly store the data

--- server.js
+++ server.js
@@ -2,36 +2,52 @@ require('dotenv').config()
 
 const OpenAI = require('openai')
 const express = require('express');
+const data = require('./data.json')
 
 const openai = new OpenAI({
     apiKey: `${process.env.OPENAI_API_KEY}`,
 });
 
 const app = express()
+app.use('/api/', express.json());
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('Hello, Node.js server! v3')
 });
 
-app.get('/completions', async (req, res) => {
+app.get('/api/reminders', (req, res) => {
+    res.send(data.reminders)
+})
+
+app.get('/api/reminders/:id', (req, res) => {
+
+    const reminderId = parseInt(req.params.id)
+    const reminder = data.reminders.find( reminder  => reminder.id == reminderId)
+    console.log(reminder)
+    if (reminder) {
+        res.send(reminder)
+    }
+    else {
+        res.status(404).send({error: 'Reminder not found' });
+    }
+});
+    
+app.get('/api/completions', async (req, res) => {
     const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         messages: [
             {
-                role: "system",
-                content: "You are a helper assistant."
+                role: 'system',
+                content: 'You are a helper assistant.'
             },
             {
-                "role": "user",
-                "content": "write a summary of how the golden state warriors and toronto raptors did recently"
+                'role': 'user',
+                'content': 'write a summary of how the golden state warriors and toronto raptors did recently'
             }
         ]
    });
 
-  console.log(completion)
-  console.log(completion.choices[0].message.content);
   res.send(`Hello, you've reached the completion page. message: ${completion.choices[0].message.content}`);
-  //res.send(completion)
 });
 
 const PORT = 3000
